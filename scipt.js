eb9d2001280b733fcaa3558c83a1e64a1959645b
owaisra <154689933+owaisra@users.noreply.github.com>
2024-01-26 18:20:47 +0500

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,23 @@
+//Arithmatic Operator
+let a= 5;
+let b= 4;
+
+console.log("a=" ,a, "& b=",b)
+console.log("a+b=", a+b)
+console.log("a-b=",a-b)
+console.log("a*b=", a*b)
+console.log("a/b=", a/b)
+console.log("a%b=", a%b)
+console.log("a**b=", a**b )
+
+
+//object joke hum banaten hain kisi bhi cheez ka 
+
+/*const product={
+titile :"ball pen",
+rating :"4 star",
+offer :"4% off",
+price :"23$",
+};
+//console.log(product);
+document.write(product)
